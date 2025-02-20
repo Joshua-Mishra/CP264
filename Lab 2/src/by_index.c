8f4f8524db607dbccedcfd5018ee1ead9ececa9f
Joshua-Mishra <162623771+Joshua-Mishra@users.noreply.github.com>
2025-02-19 23:08:48 -0500

    Add files via upload

---
+++ Lab 2/src/by_index.c
@@ -0,0 +1,31 @@
+/**
+ * -------------------------------------
+ * @file  by_index.c
+ * Lab 2 Index Functions Source Code File
+ * -------------------------------------
+ * @author name, id, email
+ *
+ * @version 2025-01-06
+ *
+ * -------------------------------------
+ */
+#include "by_index.h"
+
+void fill_values_by_index(int values[], int size) {
+
+    for(int i = 0; i < size; i++) {
+        values[i] = i + 1;
+    }
+}
+
+void fill_squares_by_index(int values[], long int squares[], int size) {
+
+    // your code here
+
+}
+
+void print_by_index(int values[], long int squares[], int size) {
+
+    // your code here
+
+}
